8773ab67d5e96df145cddc051c633ffcd21932d2
yeonnex <yeonnex@gmail.com>
2022-05-14 09:44:04 +0900

    SecurityConfig.java 에 폼로그인 기반 사용자 정의 보안 설정

---
+++ spring-security/src/main/java/me/yeonnex/myspringsecurity/MySpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package me.yeonnex.myspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringSecurityApplication.class, args);
+    }
+
+}
---
+++ spring-security/src/test/java/me/yeonnex/myspringsecurity/MySpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yeonnex.myspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
